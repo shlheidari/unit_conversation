d35dad021a1e436f8689f6df9ccf0b3ef0d8acee
shlheidari <soheilheidari289@yahoo.com>
2022-08-23 19:20:26 +0430

    adding first_page.html

--- myworld/unit_conversation/views.py
+++ myworld/unit_conversation/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('first_page.html')
+  return HttpResponse(template.render())
