0fe5f3e99d02ab2e33ec2637cb5f73063f6f0541
shlheidari <soheilheidari289@yahoo.com>
2022-08-23 20:42:57 +0430

    fixed result

--- myworld/unit_conversation/views.py
+++ myworld/unit_conversation/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
 def index(request):
   template = loader.get_template('first_page.html')
-  return HttpResponse(template.render())
+  return HttpResponse(template.render({},request))
+
+def result(request):
+  template = loader.get_template('second_page.html')
+  return HttpResponse(template.render({},request))
+
+def getnumber(request):
+    x = request.POST['first']
+    return HttpResponseRedirect(reverse('result'))
