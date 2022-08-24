078f5bd2d9645738c7409df9945f8a99d0bc395a
shlheidari <soheilheidari289@yahoo.com>
2022-08-24 09:36:50 +0430

    m to cm

--- myworld/unit_conversation/views.py
+++ myworld/unit_conversation/views.py
@@ -2,14 +2,24 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
+
+
 def index(request):
   template = loader.get_template('first_page.html')
   return HttpResponse(template.render({},request))
 
-def result(request):
-  template = loader.get_template('second_page.html')
-  return HttpResponse(template.render({},request))
+
 
 def getnumber(request):
-    x = request.POST['first']
+    global first_num
+    first_num = request.POST['first_num']
     return HttpResponseRedirect(reverse('result'))
+
+
+def result(request):
+  template = loader.get_template('second_page.html')
+  last = int(first_num) * 100
+  context = {
+    'last': last , 'first_num' : first_num
+  }
+  return HttpResponse(template.render(context,request))
