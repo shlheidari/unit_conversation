4f8e6cebcd201e01def9046d0616150f237f7c5a
shlheidari <soheilheidari289@yahoo.com>
2022-08-24 11:12:18 +0430

    no input error

--- myworld/unit_conversation/views.py
+++ myworld/unit_conversation/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-
+from django.contrib import messages
 
 
 def index(request):
@@ -20,20 +20,27 @@ def getnumber(request):
 
 def result(request):
   template = loader.get_template('second_page.html')
-  if first_unit == 'Km' : middle = int(first_num) * 1000
-  elif first_unit == 'm' : middle = int(first_num)
-  elif first_unit == 'cm' : middle = int(first_num) / 100
-  elif first_unit == 'mm' : middle = int(first_num) / 1000
+  global first_num
+  try:
+      if first_unit == 'Km' : middle = int(first_num) * 1000
+      elif first_unit == 'm' : middle = int(first_num)
+      elif first_unit == 'cm' : middle = int(first_num) / 100
+      elif first_unit == 'mm' : middle = int(first_num) / 1000
 
-  if last_unit == 'Km' : last = middle / 1000
-  elif last_unit == 'm' : last = middle
-  elif last_unit == 'cm' : last = middle * 100
-  elif last_unit == 'mm' : last = middle * 1000
+      if last_unit == 'Km' : last = middle / 1000
+      elif last_unit == 'm' : last = middle
+      elif last_unit == 'cm' : last = middle * 100
+      elif last_unit == 'mm' : last = middle * 1000
+      context = {
+        'last': last , 'first_num' : first_num , 'first_unit' : first_unit , 'last_unit' : last_unit
+      }
+      return HttpResponse(template.render(context,request))
 
-  context = {
-    'last': last , 'first_num' : first_num , 'first_unit' : first_unit , 'last_unit' : last_unit
-  }
-  return HttpResponse(template.render(context,request))
+  except:
+      messages.add_message(request, messages.ERROR, 'please enter a number')
+      return HttpResponseRedirect(reverse('index'))
+
+
 
 def back(request):
     return HttpResponseRedirect(reverse('index'))
